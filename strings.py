3a9f15daeba5c7bd59ed3161dd31ec7d6d651e5d
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-09-20 08:39:56 +0300

    new_string functionality

--- strings.py
+++ strings.py
@@ -1,3 +1,6 @@
 # Replacing the contents of a string by creating a new variable of the same previous variable
 message = "A kong string with a silly typo"
 print(message)
+new_message = message[0:2] + "l" + message[3:]
+print("The replaced string reads:")
+print(new_message)
