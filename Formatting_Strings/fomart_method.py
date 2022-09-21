0a1e0ba712962d743ed0dfdfb3a540efcc87b83f
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-09-21 11:23:21 +0300

    Value placeholder in the placeholder within the string

--- Formatting_Strings/fomart_method.py
+++ Formatting_Strings/fomart_method.py
@@ -1,3 +1,4 @@
 name = "Denzel"
 number = len(name) * 5
-print("Hello {}, your lucky number is {}".format(name, number))
+# print("Hello {}, your lucky number is {}".format(name, number))
+print("Your lucky number is {number}, {name}".format(name=name, number=number))
