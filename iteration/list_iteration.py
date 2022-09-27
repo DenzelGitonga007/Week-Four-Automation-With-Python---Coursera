1606e9402b42bc90ba24f18f2f2a3783c7588ac4
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-09-27 11:26:11 +0300

    using the enumerate function

--- iteration/list_iteration.py
+++ iteration/list_iteration.py
@@ -3,3 +3,8 @@ animals, chars = ["monkey", "lion", "zebra", "dolphin"], 0
 for animal in animals:
     chars += len(animal) #iterate over each string value, finding it's length as chars
 print("Total characters in the list: {}, average length of the strings: {}".format(chars, chars/len(animals)))
+
+# using the enumerate function to obtain the index of an element and it's value-- a tuple of index, and value
+winners = ["Denzel", "Catherine", "Junne", "Francis"]
+for index, person in enumerate(winners):
+    print("{} - {}".format(index + 1,person))
