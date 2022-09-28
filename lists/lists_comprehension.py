8a3af41d9f07ea0964c05bfecd15b15e45bb9369
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-09-28 15:09:56 +0300

    Finding the length of strings in a list using the list comprehension

--- lists/lists_comprehension.py
+++ lists/lists_comprehension.py
@@ -1,3 +1,10 @@
 # appending multiples of a number
 multiples = [ x*7 for x in range(0,11)]
-print(multiples)
+num = 7
+print("Testing for list comprehension to append multiples of a number ({}): ".format(num) + str(multiples))
+
+# finding the length of the strings in a list
+languages = ["Python", "Java", "Php", "HTML"]
+lengths = [len(language) for language in languages]
+len_in_list_compreh = "Using list comprehension to find the length of strings in a list eg: "
+print("{}".format(len_in_list_compreh) + str(lengths))
