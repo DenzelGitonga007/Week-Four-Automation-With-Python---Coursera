bd7473affc7686874e83805f06f2778e02524064
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-09-28 15:19:23 +0300

    Using conditional clauses with lists comprehesion

--- lists/lists_comprehension.py
+++ lists/lists_comprehension.py
@@ -1,6 +1,6 @@
 # appending multiples of a number
 multiples = [ x*7 for x in range(0,11)]
-num = 7
+num = 7 # the divisor being tested
 print("Testing for list comprehension to append multiples of a number ({}): ".format(num) + str(multiples))
 
 # finding the length of the strings in a list
@@ -8,3 +8,8 @@ languages = ["Python", "Java", "Php", "HTML"]
 lengths = [len(language) for language in languages]
 len_in_list_compreh = "Using list comprehension to find the length of strings in a list eg: "
 print("{}".format(len_in_list_compreh) + str(lengths))
+
+# Using conditional clause in list comprehension
+# To get all the numbers divisible by 3 from 0 to 100
+three_multiples = [z for z in range(0,101) if z % 3 == 0]
+print(three_multiples)
