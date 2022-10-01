38ad657f7a4e9cca286565c2e9610a8a95d91756
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-10-01 12:23:03 +0300

    Adding an entry

---
+++ Dictionaries/adding_replacing_removing_files.py
@@ -0,0 +1,7 @@
+file_list = {"jpg": 12, "txt": 14, "py": 24, "pdf": 5}
+print("This is the initial file list")
+print(file_list)
+# adding an entry into the dictionary
+file_list["csv"] = 10
+print("After adding an entry onto the list")
+print(file_list)
