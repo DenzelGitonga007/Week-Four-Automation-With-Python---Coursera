be726a968ed6330e53f27edc8a998cd041308d92
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-10-01 12:05:17 +0300

    In keyword

--- Dictionaries/accessing_values.py
+++ Dictionaries/accessing_values.py
@@ -3,3 +3,6 @@ files_list = {"jpg": 12, "txt": 14, "py": 24, "pdf": 5}
 print(files_list["txt"])
 print(files_list["py"])
 
+# Using the in key-word check if a certain key exists in the dictionary
+checker = "py" in files_list
+print(checker)
