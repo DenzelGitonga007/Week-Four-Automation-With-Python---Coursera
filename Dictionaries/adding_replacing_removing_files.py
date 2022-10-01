35281ab52aa18517fc4b9d05c8edafb798676986
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-10-01 12:25:10 +0300

    Replacing the value of a key

--- Dictionaries/adding_replacing_removing_files.py
+++ Dictionaries/adding_replacing_removing_files.py
@@ -1,7 +1,11 @@
 file_list = {"jpg": 12, "txt": 14, "py": 24, "pdf": 5}
 print("This is the initial file list")
 print(file_list)
-# adding an entry into the dictionary
+# Adding an entry into the dictionary
 file_list["csv"] = 10
 print("After adding an entry onto the list")
+print(file_list)
+# Replacing the value of an entry
+file_list["jpg"] = 14
+print("Changing the value of jpg from 12 to 14")
 print(file_list)
