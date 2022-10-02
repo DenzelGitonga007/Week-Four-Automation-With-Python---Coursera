5718b7c3ef16d93a975e526912777a7ffb67f660
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-10-02 21:41:55 +0300

    Using the item function to access both the keys and their values

--- Dictionaries/iteration.py
+++ Dictionaries/iteration.py
@@ -3,3 +3,6 @@ pass_list = {"BIT 314": 14, "BCS 313": 19, "ESM 101": 1}
 for courses in pass_list:
     print(courses)
 
+# To iterate over both the keys and their values, use the item function
+for extension, amount in pass_list.items():
+    print("There are {} values with the .{} extension".format(amount, extension))
